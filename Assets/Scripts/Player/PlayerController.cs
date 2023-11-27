7b3002c45f0c16c6c0c088e42b864871f70babb8
LemonMaster66 <539993@chrome.escg.ac.uk>
2023-11-27 01:16:17 +0000

    Slerp Rotation
    added Quaternion.Slerp() to smooth out the rotation on the player when it goes in and out of rolling mode
    
    Also added Unused Functionality for different Roll and Get-Up Types

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,10 +13,12 @@ public class PlayerController : MonoBehaviour
     public float JumpForce = 8;
     public float Gravity = 100;
 
-    public bool Grounded;
-    public bool Rolling;
+    public bool Grounded       = true;
+    public bool Rolling        = false;
+    public bool RollingStorage = true;
 
     private Rigidbody rb;
+    private Timers timings;
 
     [Header("Turning Physics")]
     [Tooltip("Turning Speeds Increase With Forward Momentum (Recomended)")]
@@ -38,7 +42,7 @@ public class PlayerController : MonoBehaviour
     public int VolumeDivide = 60;
 
     [Header("Debug Stats")]
-    public float PlayerVelocity;
+    public Vector3 PlayerVelocity;
     public float ForwardVelocityMagnitude;
     public float turnSpeed;
     public float FallingVelocity;
@@ -46,29 +50,28 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
+        timings = GetComponent<Timers>();
         rb = GetComponent<Rigidbody>();
-        GetComponent<Rigidbody>().useGravity = false;
-        GameObject cameraObject = GameObject.Find("Camera");
-        Camera = cameraObject.transform;
+        rb.useGravity = false;
+        Camera = GameObject.Find("Camera").transform;
     }
 
     void FixedUpdate()
     {   
         #region Debug Stats
             int decimals = 2;
-            PlayerVelocity = rb.velocity.magnitude;
-            PlayerVelocity = (float)System.Math.Round(PlayerVelocity, decimals);
-            ForwardVelocityMagnitude = (float)System.Math.Round(ForwardVelocityMagnitude, decimals);
-            turnSpeed = (float)System.Math.Round(turnSpeed, decimals);
+            PlayerVelocity = rb.velocity;
+            PlayerVelocity.x = (float)Math.Round(PlayerVelocity.x, decimals);
+            PlayerVelocity.y = (float)Math.Round(PlayerVelocity.y, decimals);
+            PlayerVelocity.z = (float)Math.Round(PlayerVelocity.z, decimals);
+            turnSpeed = (float)Math.Round(turnSpeed, decimals);
         #endregion
         //**********************************
         #region Sfx
             //Rolling Audio
             if (Grounded)
             {
-                XVolume = Mathf.Sqrt(rb.velocity.x * rb.velocity.x);
-                ZVolume = Mathf.Sqrt(rb.velocity.z * rb.velocity.z);
-                RollingAudio.volume = XVolume/VolumeDivide + ZVolume/VolumeDivide;
+                RollingAudio.volume = rb.velocity.magnitude/VolumeDivide;
             }
             else
             {
@@ -76,9 +79,50 @@ public class PlayerController : MonoBehaviour
 
                 FallingVelocity = rb.velocity.y*-1 / VolumeDivide;
                 LandAudio.volume = FallingVelocity;
+                LandAudio.Play();
             }
         #endregion
         //**********************************
+        #region PerFrame Calculations
+            Vector3 CamF = Camera.forward;
+            Vector3 CamR = Camera.right;
+            CamF.y = 0;
+            CamR.y = 0;
+            CamF = CamF.normalized;
+            CamR = CamR.normalized;
+
+            // Calculate the Forward velocity magnitude
+            Vector3 ForwardVelocity = Vector3.Project(rb.velocity, CamF);
+            ForwardVelocityMagnitude = ForwardVelocity.magnitude;
+            ForwardVelocityMagnitude = (float)System.Math.Round(ForwardVelocityMagnitude, decimals);
+
+            // Calculate the Forward Angle
+            float targetAngle = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
+            Quaternion toRotation = Quaternion.Euler(0f, targetAngle, 0f);
+        #endregion
+        //**********************************
+
+
+        //Walking
+        if(!Rolling && Grounded && timings.RollStorageTimer == 0)
+        {
+            if(RollingStorage)
+            {
+                speed = 200;
+                maxSpeed = 20;
+                RollingStorage = false;
+            }
+            rb.freezeRotation = true;
+            if(rb.velocity != Vector3.zero) transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 0.25f);
+            //transform.rotation = Quaternion.Euler(0,transform.rotation.y,0);
+        }
+
+        //Roll Storage Period
+        if(!Rolling && RollingStorage && !Grounded)
+        {
+            rb.freezeRotation = true;
+            transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 0.06f);
+        }
 
         //Extra Gravity
         rb.AddForce(Physics.gravity * Gravity /10);
@@ -94,16 +138,6 @@ public class PlayerController : MonoBehaviour
             rb.velocity = newVelocity;
         }
 
-        Vector3 CamF = Camera.forward;
-        Vector3 CamR = Camera.right;
-        CamF.y = 0;
-        CamR.y = 0;
-        CamF = CamF.normalized;
-        CamR = CamR.normalized;
-
-        // Calculate the forward velocity magnitude
-        Vector3 ForwardVelocity = Vector3.Project(rb.velocity, CamF);
-        ForwardVelocityMagnitude = ForwardVelocity.magnitude;
 
         Vector3 movement = (CamF * movementY + CamR * movementX).normalized;
         turnSpeed = turnSpeedFactor * ForwardVelocityMagnitude;
@@ -132,9 +166,24 @@ public class PlayerController : MonoBehaviour
         if(context.started)
         {
             Rolling = true;
+            RollingStorage = true;
+            rb.freezeRotation = false;
             if(Grounded)
             {
-                rb.freezeRotation = false;
+                speed = 150;
+                maxSpeed = 200;
+                if(rb.velocity.magnitude > 0.1) //Dive Roll
+                {
+                    Debug.Log("Dive Roll");
+                }
+                else //Get Down Roll
+                {
+                    Debug.Log("Get Down Roll");
+                }
+            }
+            else if(!Grounded && Rolling && RollingStorage) //Flip Over
+            {
+                Debug.Log("Flip Over");
             }
         }
         //Stop Rolling
@@ -143,7 +192,20 @@ public class PlayerController : MonoBehaviour
             Rolling = false;
             if(Grounded)
             {
-                rb.freezeRotation = true;
+                if(rb.velocity.magnitude > 0.1) //Bounce Up
+                {
+                    Debug.Log("Bounce Up");
+                    rb.AddForce(Vector3.up * 12, ForceMode.VelocityChange);
+                    timings.RollStorageTimer = 0.1f;
+                }
+                else //Get Up
+                {
+                    Debug.Log("Get Up");
+                }
+            }
+            else if(!Grounded && !Rolling && RollingStorage) //Flip Upright
+            {
+                Debug.Log("Flip Upright");
             }
         }
     }
---
+++ Assets/Scripts/Player/Timers.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timers : MonoBehaviour
+{
+    public float RollStorageTimer;
+
+    private void RollStorageTimerFunction()
+    {
+        RollStorageTimer -= Time.deltaTime;
+        if(RollStorageTimer <= 0f) RollStorageTimer = 0;
+        return;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(RollStorageTimer > 0) RollStorageTimerFunction();
+    }
+}
